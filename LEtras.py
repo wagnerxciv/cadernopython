4c5bc27ffb733c005c23a85dd866bdfc473ed63b
wagnerxciv <125404234+wagnerxciv@users.noreply.github.com>
2023-05-10 15:00:36 -0300

    LEtras.py

---
+++ LEtras.py
@@ -0,0 +1,11 @@
+letrinha = input ( "Digite uma letra: \n " )
+if  letrinha == "a" :
+    print ( "Voce digitou uma vogal" )
+elif  letrinha == "e" :
+    print ( "Voce digitou uma vogal" )
+elif  letrinha == "o" :
+    print ( "Voce digitou uma vogal" )
+elif  letrinha == "u" :
+    print ( "Voce digitou uma vogal" )
+senão :
+    print ( "Voce digitou uma consoante" )
