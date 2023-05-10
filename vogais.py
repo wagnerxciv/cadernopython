80d52c05d250b0a29d3498dabc3a0fb06cccfd18
wagnerxciv <125404234+wagnerxciv@users.noreply.github.com>
2023-05-10 15:04:06 -0300

    vogais.py

---
+++ vogais.py
@@ -0,0 +1,6 @@
+letra = input ( "Digite uma letra: \n " )
+vogais = [ "a" , "e" , "i" , "o" , "u" ]
+se  letra  em  vogais :
+    print ( "A letra " , letra , "E uma vogal" )
+senão :
+    print ( "A letra" , letra , "E uma consoante" )
